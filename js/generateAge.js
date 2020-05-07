688d500becbd041c642b8408499233abf6c6c536
Fred Ojure <fredojure@hotmail.com>
2020-05-07 20:31:09 +0300

    Clean up of items used to display vale. Add <p> elements to display results

--- js/generateAge.js
+++ js/generateAge.js
@@ -15,15 +15,12 @@ function getValues() {
 
   if (dd < 0 || dd > 31) {
     alert("invalid date");
-    
   }
   if (mm < 0 || mm > 12) {
     alert("invalid date");
-    
   }
   if (yy > 2020) {
     alert("future date");
-    
   }
 
   //extract century and year
@@ -32,19 +29,25 @@ function getValues() {
 
   //calculate day of week - Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
   var dow = Math.floor(
-    (cc/4 - 2 * cc - 1 + (5 * dyy) / 4 + (26 * (mm + 1)) / 10 + dd) % 7
+    (cc / 4 - 2 * cc - 1 + (5 * dyy) / 4 + (26 * (mm + 1)) / 10 + dd) % 7
   );
 
   //validate gender
   var lcGender = gender.toLowerCase();
   if (lcGender != "male" && lcGender != "female") {
     alert("invalid gender");
-    
   }
 
   //Generate name
-  var maleNames = ["Kwasi","Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var femaleNames = ["Akoswa", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+  var dayOfWeekArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",  "Friday", "Saturday"];
+
+  var dayName = dayOfWeekName(dow);
+
+  function dayOfWeekName(dayOfWeek) {
+    return dayOfWeekArr[dow];
+  }
 
   var genName = akanName(lcGender, dow);
 
@@ -54,11 +57,14 @@ function getValues() {
     } else {
       return femaleNames[dow];
     }
-  };
+  }
 
-  //display values in <p> elements
-  var dateDisplay = document.getElementById("display-date");
-  var genderDisplay = document.getElementById("display-gender");
-  dateDisplay.innerHTML = dd + " " + mm + " " + yy + " " + dyy + " " + cc + " " + dow + " " + genName;
-  genderDisplay.innerHTML = lcGender;
+  //display values in web page
+  var akanFinalName = document.getElementById("akan-name");
+  var akanSecondFinalName = document.getElementById("akan-name2");
+  var dayofWeekDisplay = document.getElementById("dayOfWeek");
+
+  akanFinalName.innerHTML = genName;
+  akanSecondFinalName.innerHTML = genName;
+  dayofWeekDisplay.innerHTML = dayName;
 }
